283f8f0694326c9ca6c209ab623c82059d7a5583
kpalaric27 <kpalaric27@gmail.com>
2017-06-29 23:43:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app= Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "UPS on the GO!"
+@app.route("/", methods=['GET', 'POST'])
+def handle_data():
+    print request.values
+    return "hello world"
 
+'''
+@app.route('/result', methods =['GET', 'POST'])
+def result():
+    if request.method== 'POST':
+        result= request.form['textData']
+        result=result[::-1]
+        return render_template("result.html", result=result)
+'''
 if __name__ == '__main__':
-    app.run()
+    app.debug= True
+    app.run('0.0.0.0', 8080)
+    app.run(debug= True)
